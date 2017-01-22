044e6f28b7cf11bb9aaa6187126f99bff0813cb0
Henry Doan <henrydoan96@gmail.com>
2017-01-22 14:42:10 -0700

    added the collatz conjecture

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Ruby Hailstone Sequence
 The hailstone sequence is simple, if and integer is even then divide by 2 and if it is odd then times by 3 and add one. 
 
+Eventually if the sequence continue, it will eventually converges to the number 1, this this phenomenon is called the Collatz Conjecture.
+
 This program in made in ruby and works in the terminal with :
 ```ruby hailstone.rb```
--- hailstone.rb
+++ hailstone.rb
@@ -3,6 +3,7 @@
 @arr = [];
 @iterations = 1;
 
+# TODO what to do if the number never reaches one in the interations
 def main
 	puts "Please input a positive integer, all other integer will be turned into positive integers: "
 	@n = (gets.strip.to_i).abs
@@ -13,7 +14,9 @@ def main
 
 	puts "Here are the next numbers in the sequence for the next #{@iterations} times: "
 	while @count < @iterations
-		if @n %2 == 0 
+		if @n == 1
+			break
+		elsif @n %2 == 0 
 			@n = @n / 2
 			@arr << @n
 			@count += 1
@@ -24,6 +27,9 @@ def main
 		end
 	end
 	puts "#{@arr.join(", ")}"
+
+	puts "Your number reaches 1 with #{@count} iterations"
+	puts "This phenomenon is called the Collatz Conjecture which states that this sequence will eventually converge to 1 with any give value."
 
 	again
 end
